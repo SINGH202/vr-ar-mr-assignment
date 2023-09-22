40d9606f803797d22b349239d45790d6010ebbf9
ANURAG SINGH <apk.anurag.singh@gmail.com>
2023-09-22 17:27:33 +0530

    Added bg image to second screen.

---
+++ src/pages/controls.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Controls() {
+  return (
+    <main className={`flex min-h-screen flex-col`}>
+      <div
+        style={{ backgroundImage: 'url("/images/background.png")' }}
+        className="h-[100vh] bg-contain">
+            
+        </div>
+    </main>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,30 @@
 import { Inter } from "next/font/google";
 import { Timer } from "@/components/Timer";
 import { BackgroundVideo } from "@/components/BackgroundVideo";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [seconds, setSeconds] = useState(0);
+  const router = useRouter();
+  useEffect(() => {
+    if (seconds < 9) {
+      const timer = setInterval(() => {
+        setSeconds(seconds + 1);
+      }, 1000);
+
+      return () => clearInterval(timer);
+    } else {
+      router.push("/controls");
+    }
+  }, [seconds]);
   return (
     <main className={`flex min-h-screen flex-col ${inter.className} `}>
-      <div className="">
-        <BackgroundVideo>
-          <Timer />
-        </BackgroundVideo>
-      </div>
+      <BackgroundVideo>
+        <Timer />
+      </BackgroundVideo>
     </main>
   );
 }
